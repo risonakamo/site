c89449f89f901738181a78587baaab1c6d4a7ed1
risonakamo <hmykanae@gmail.com>
2018-09-11 14:18:48 -0400

    dynamic entry generation for now

--- index.js
+++ index.js
@@ -2,5 +2,26 @@ window.onload=main;
 
 function main()
 {
+    getData("entrydata.json",(data)=>{
+        ReactDOM.render(React.createElement(React.Fragment,null,
+            data.map((x,i)=>{
+                return React.createElement(Entry,{key:i,data:x});
+            })
+        ),document.querySelector(".entry-holder"));
+    });
+}
 
+function getData(file,callback)
+{
+    var r=new XMLHttpRequest();
+    r.open("GET",file);
+
+    r.onreadystatechange=()=>{
+        if (r.readyState==4)
+        {
+            callback(JSON.parse(r.response));
+        }
+    };
+
+    r.send();
 }
