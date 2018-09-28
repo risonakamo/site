aafad2279b8a44db39b4f80413059713b8839592
risonakamo <hmykanae@gmail.com>
2018-09-27 23:40:26 -0400

    鮫

--- js/thefooter.js
+++ js/thefooter.js
@@ -2,18 +2,23 @@ class TheFooter extends React.Component {
   constructor(props) {
     super(props);
     this.expandProfile = this.expandProfile.bind(this);
+    this.state = {//profileShown:0*
+    };
     this.profileImg = React.createRef();
-    this.profileImgImg = React.createRef(); //this.profileShown*
+    this.profileImgImg = React.createRef();
   }
 
   expandProfile(e) {
     e.preventDefault();
 
-    if (this.profileShown) {
+    if (this.state.profileShown) {
       return;
     }
 
-    this.profileShown = 1;
+    this.setState({
+      profileShown: 1
+    });
+    var animationDuration = 500;
     this.profileImg.current.animate([{
       height: "0px",
       opacity: 0
@@ -21,16 +26,31 @@ class TheFooter extends React.Component {
       height: this.profileImgImg.current.scrollHeight + "px",
       opacity: 1
     }], {
-      duration: 800,
+      duration: animationDuration,
       fill: "forwards",
       easing: "cubic-bezier(0.215, 0.61, 0.355, 1)" //easeoutcubic
 
     });
+    var count = 0;
+    var scrollDown = setInterval(() => {
+      window.scrollTo(0, document.documentElement.scrollHeight);
+      count += 10;
+
+      if (count == animationDuration) {
+        clearInterval(scrollDown);
+      }
+    }, 10);
   }
 
   render() {
-    return React.createElement(React.Fragment, null, React.createElement("p", null, "Website made by: ", React.createElement("a", {
-      href: "",
+    var sharkButtonClass = "sharkbutton";
+
+    if (this.state.profileShown) {
+      sharkButtonClass = "sharkbutton clicked";
+    }
+
+    return React.createElement(React.Fragment, null, React.createElement("p", null, "Website made by: ", React.createElement("span", {
+      className: sharkButtonClass,
       onClick: this.expandProfile
     }, "Me"), " of course. Who else?"), React.createElement("div", {
       className: "profile-img",
--- js/thefooter.jsx
+++ js/thefooter.jsx
@@ -5,36 +5,59 @@ class TheFooter extends React.Component
     super(props);
     this.expandProfile=this.expandProfile.bind(this);
 
+    this.state={
+      //profileShown:0*
+    };
+
     this.profileImg=React.createRef();
     this.profileImgImg=React.createRef();
-    //this.profileShown*
   }
 
   expandProfile(e)
   {
     e.preventDefault();
 
-    if (this.profileShown)
+    if (this.state.profileShown)
     {
       return;
     }
 
-    this.profileShown=1;
+    this.setState({profileShown:1});
+
+    var animationDuration=500;
 
     this.profileImg.current.animate([
       {height:"0px",opacity:0},
       {height:this.profileImgImg.current.scrollHeight+"px",opacity:1}
     ],{
-      duration:800,
+      duration:animationDuration,
       fill:"forwards",
       easing:"cubic-bezier(0.215, 0.61, 0.355, 1)" //easeoutcubic
     });
+
+    var count=0;
+
+    var scrollDown=setInterval(()=>{
+      window.scrollTo(0,document.documentElement.scrollHeight);
+      count+=10;
+
+      if (count==animationDuration)
+      {
+        clearInterval(scrollDown);
+      }
+    },10);
   }
 
   render()
   {
+    var sharkButtonClass="sharkbutton";
+    if (this.state.profileShown)
+    {
+      sharkButtonClass="sharkbutton clicked";
+    }
+
     return (<>
-      <p>Website made by: <a href="" onClick={this.expandProfile}>Me</a> of course. Who else?</p>
+      <p>Website made by: <span className={sharkButtonClass} onClick={this.expandProfile}>Me</span> of course. Who else?</p>
       <div className="profile-img" ref={this.profileImg}><img src="img/me.png" ref={this.profileImgImg}/></div>
       <a href="https://github.com/risonakamo/site">This site's github</a>
     </>);
